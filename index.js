c686f49bc4a081b8d2fe9b591bde4d9bd0c8dfc1
maayami <mayank16meena@gmail.com>
2021-02-24 22:34:47 +0530

    Data fetch test on FakeAPI

--- index.js
+++ index.js
@@ -7,41 +7,17 @@ const io = require("socket.io")(http, {
 });
 
 io.on("connection", (socket) => {
-  // socket.on("new-user-joined", (userName) => {
-  //   users[socket.id] = userName;
-  //   socket.broadcast.emit("user-joined", userName);
-  // });
-
-  // socket.on("send", (message) => {
-  //   socket.broadcast.emit("receive", {
-  //     message: message,
-  //     userName: users[socket.id],
-  //   });
-  // });
-  // socket.on("disconnect", (message) => {
-  //   socket.broadcast.emit("leave", {
-  //     message: message,
-  //     userName: users[socket.id],
-  //   });
-  //   delete users[socket.id];
-  // });
   let chatRoomId = "";
   socket.on("user-active", (activeUser) => {
     chatRoomId = activeUser.chatRoomId;
-    // console.log(chatRoomId);
     socket.join(chatRoomId);
     socket.broadcast.to(chatRoomId).emit("user-online", activeUser);
   });
 
   socket.on("chat-message", (chat) => {
-    // console.log(chatRoomId);
     socket.broadcast.to(chatRoomId).emit("receive-message", chat);
   });
-  socket.on("disconnect", () => {
-    // console.log("user disconnected");
-  });
+  socket.on("disconnect", () => {});
 });
 
-http.listen(4000, () => {
-  // console.log("listeniing on port  4000");
-});
+http.listen(4000, () => {});
